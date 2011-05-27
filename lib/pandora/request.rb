3777eba8193b0b77c5d368ad113e0e1edec937f2
Anil <anilv87@gmail.com>
2011-05-27 16:13:00 -0400

    Added bookmarked_artists,stations, now_playing, recent_activity

--- lib/pandora/request.rb
+++ lib/pandora/request.rb
@@ -1,9 +1,9 @@
 module Pandora
   module Request
-    BASE_FEED = "http://feeds.pandora.com/feeds/people"
+    BASE_FEED = "http://feeds.pandora.com/feeds/people/"
     
     def request(user, method)
-      doc = Nokogiri::XML(open("http://feeds.pandora.com/feeds/people/#{user}/#{method}.xml"))
+      doc = Nokogiri::XML(open("#{BASE_FEED}#{user}/#{method}.xml"))
     end
   end
 end
--- lib/pandora/user.rb
+++ lib/pandora/user.rb
@@ -8,8 +8,9 @@ module Pandora
       @user = user.strip
     end
     
-    def favorite_songs(songs=[])
-      doc = @user.request(@user, "favorites")
+    def bookmarked_songs
+      doc = request(@user, "favorites")
+      songs = []
       doc.xpath('//rss/channel/item').each do |node|
         songs << { :title       => node.xpath('title').text,
                    :link        => node.xpath('link').text,
@@ -20,6 +21,64 @@ module Pandora
                    :album       => node.xpath('mm:Album/dc:title').text,
                    :artwork     => node.xpath('pandora:albumArtUrl').text,
                    :station     => node.xpath('pandora:stationLink').text }
+      end
+      songs
+    end
+    
+    def bookmarked_artists
+      doc = request(@user, "favoriteartists")
+      artists = []
+      doc.xpath('//rss/channel/item').each do |node|
+        artists << { :title       => node.xpath('title').text,
+                     :link        => node.xpath('link').text,
+                     :description => node.xpath('description').text,
+                     :date        => node.xpath('pubDate').text,
+                     :artist      => node.xpath('mm:Artist/dc:title').text,
+                     :artwork     => node.xpath('pandora:artistPhotoUrl').text,
+                     :station     => node.xpath('pandora:stationLink').text }
+      end
+      artists
+    end
+    
+    def stations
+      doc = request(@user, "stations")
+      stations = []
+      doc.xpath('//rss/channel/item').each do |node|
+        stations << { :title       => node.xpath('title').text,
+                      :link        => node.xpath('link').text,
+                      :description => node.xpath('description').text,
+                      :date        => node.xpath('pubDate').text,
+                      :artwork     => node.xpath('pandora:stationAlbumArtImageUrl').text }
+      end
+      stations
+    end
+    
+    def now_playing
+      doc = request(@user, "nowplaying")
+      station = []
+      doc.xpath('//rss/channel/item').each do |node|
+        station << { :title       => node.xpath('title').text,
+                     :link        => node.xpath('link').text,
+                     :description => node.xpath('description').text,
+                     :date        => node.xpath('pubDate').text,
+                     :artwork     => node.xpath('pandora:stationAlbumArtImageUrl').text }
+      end
+    end
+    
+    def recent_activity
+      doc = request(@user, "recentactivity")
+      items = []
+      doc.xpath('//rss/channel/item').each do |node|
+        items << { :title       => node.xpath('title').text,
+                   :link        => node.xpath('link').text,
+                   :description => node.xpath('description').text,
+                   :date        => node.xpath('pubDate').text,
+                   :track       => node.xpath('mm:Track/dc:title').text,
+                   :artist      => node.xpath('mm:Artist/dc:title').text,
+                   :album       => node.xpath('mm:Album/dc:title').text,
+                   :artwork     => node.xpath('pandora:albumArtUrl').text,
+                   :station     => node.xpath('pandora:stationLink').text }
+      end
     end
   end
 end
